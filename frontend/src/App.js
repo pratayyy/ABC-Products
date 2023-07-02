6973262da31f78e3d85952c5f15f74dacfdf80ab
pratayyy <2028026@kiit.ac.in>
2023-07-02 11:21:02 +0530

    Header added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,39 @@
+import React from "react";
+import Header from "./components/Header";
+
 export default function App() {
+  const scrollbarStyle = {
+    width: "12px",
+  };
+
+  const scrollbarTrackStyle = {
+    backgroundColor: "#666666",
+  };
+
+  const scrollbarThumbStyle = {
+    backgroundColor: "#505050",
+    borderRadius: "100px",
+  };
+
   return (
     <>
-      <h1>Hello World</h1>
+      <style>
+        {`
+          ::-webkit-scrollbar {
+            width: ${scrollbarStyle.width}
+          }
+
+          ::-webkit-scrollbar-track {
+            background-color: ${scrollbarTrackStyle.backgroundColor}
+          }
+
+          ::-webkit-scrollbar-thumb {
+            background-color: ${scrollbarThumbStyle.backgroundColor},
+            border-radius: ${scrollbarThumbStyle.borderRadius}
+          }
+        `}
+      </style>
+      <Header />
     </>
   );
 }
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import abclogo from "../assets/abclogo.svg";
+import hrclogo from "../assets/hrclogo.svg";
+
+export default function Header() {
+  const headerStyle = {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-between",
+    height: "90px",
+    padding: "12px 8px 4px 8px",
+  };
+
+  const logoStyle = {
+    position: "relative",
+  };
+
+  const abcLogoStyle = {
+    width: "240px",
+  };
+
+  const hrcLogoStyle = {
+    width: "160px",
+    position: "absolute",
+    left: "50%",
+    transform: "translate(-50%)",
+  };
+
+  const invoiceHeadingStyle = {
+    color: "rgb(247, 52, 52)",
+    fontFamily: "Roboto, sans-serif",
+    fontSize: "24px",
+    fontWeight: "500",
+  };
+
+  return (
+    <div style={headerStyle}>
+      <div style={logoStyle}>
+        <img src={abclogo} alt="abclogo" style={abcLogoStyle} />
+        <img src={hrclogo} alt="hrclogo" style={hrcLogoStyle} />
+      </div>
+      <span style={invoiceHeadingStyle}>Invoice List</span>
+    </div>
+  );
+}
